63c365c9f810233c839afbda0c3172ae269e29a9
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2025-05-05 02:21:12 +0300

    Added factory methods for simplicity.

--- comp/solvers/__init__.py
+++ comp/solvers/__init__.py
@@ -1,5 +1,15 @@
 from .base import BaseSolver
+from .center import CenterLinearFirst, CenterLinearSecond
+from .element import ElementLinearFirst, ElementLinearSecond, ElementCombinatorialFirst
+from .factories import element_solver_fabric, center_solver_fabric
 
 __all__ = [
     "BaseSolver",
+    "ElementLinearFirst",
+    "ElementLinearSecond",
+    "ElementCombinatorialFirst",
+    "CenterLinearFirst",
+    "CenterLinearSecond",
+    "element_solver_fabric",
+    "center_solver_fabric",
 ]
---
+++ comp/solvers/factories.py
@@ -0,0 +1,46 @@
+from comp.models import CenterType, CenterData, ElementData, ElementType
+from comp.solvers import BaseSolver
+
+
+def element_solver_fabric(data: ElementData) -> BaseSolver:
+    """
+    Factory function to create an element solver based on the specified type.
+
+    Args:
+        data: The data associated with the element.
+
+    Returns:
+        An instance of the appropriate solver class based on the element type.
+    """
+
+    from comp.solvers import ElementLinearFirst, ElementLinearSecond
+
+    if data.config.type == ElementType.DECENTRALIZED:
+        return ElementLinearFirst(data)
+    elif data.config.type == ElementType.NEGOTIATED:
+        return ElementLinearSecond(data)
+    else:
+        raise ValueError(f"Unknown element type for factory: {data.config.type}")
+
+
+def center_solver_fabric(data: CenterData) -> BaseSolver:
+    """
+    Factory function to create a center solver based on the specified type.
+
+    Args:
+        data: The data associated with the center.
+
+    Returns:
+        An instance of the appropriate solver class based on the center type.
+    """
+
+    from comp.solvers import CenterLinearFirst
+
+    if data.config.type == CenterType.STRICT_PRIORITY:
+        return CenterLinearFirst(data)
+    elif data.config.type == CenterType.GUARANTEED_CONCESSION:
+        raise NotImplementedError("Guaranteed Concession strategy is not implemented yet.")
+    elif data.config.type == CenterType.WEIGHTED_BALANCE:
+        raise NotImplementedError("Weighted Balance strategy is not implemented yet.")
+    else:
+        raise ValueError(f"Unknown center type: {data.config.type}")
