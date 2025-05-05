026b87417920be9ed32c0444bbf79127f97118f1
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2025-05-06 00:49:50 +0300

    Optimized imports.

--- comp/solvers/center/linear/first.py
+++ comp/solvers/center/linear/first.py
@@ -1,9 +1,7 @@
 from comp.models import CenterData
 from comp.solvers.core import CenterSolver
-
 from comp.solvers.factories import element_solver_fabric
-from comp.utils import (assert_valid_dimensions, assert_non_negative, assert_positive, tab_out, stringify,
-                        copy_element_coeffs, lp_sum)
+from comp.utils import copy_element_coeffs, lp_sum
 
 
 class CenterLinearFirst(CenterSolver):
@@ -15,7 +13,7 @@ class CenterLinearFirst(CenterSolver):
         self.element_solvers = [element_solver_fabric(element) for element in data.elements]
         self.center_element_solvers = [element_solver_fabric(copy_element_coeffs(element, data.coeffs_functional[e]))
                                        for e, element in enumerate(data.elements)]
-        self.center_element_results = list()
+        self.f_c_opt = list()
 
     def add_constraints(self) -> None:
         """Add constraints to the element's solvers."""
@@ -25,59 +23,12 @@ class CenterLinearFirst(CenterSolver):
 
             # Optimality Equality Constraint: d_e^T * y_e = f_c_opt_e
             element_solver.solver.Add(
-                lp_sum(self.data.coeffs_functional[e][i] * element_solver.get_plan(i)
+                lp_sum(self.data.coeffs_functional[e][i] * element_solver.get_plan_component(i)
                        for i in range(element_solver.data.config.num_decision_variables))
-                == self.center_element_results[e][0]
+                == self.f_c_opt[e]
             )
 
     def get_optimal_solutions(self) -> None:
         """Get optimal solutions for the element's solvers."""
 
-        self.center_element_results = [solver.setup() or solver.solve() for solver in self.center_element_solvers]
-
-    def print_results(self) -> None:
-        """Print the results of the optimization for the center problem."""
-
-        center_functional = 0
-
-        tab_out(f"\nInput data for center {stringify(self.data.config.id)}", (
-            ("Center Type", stringify(self.data.config.type)),
-            ("Center ID", stringify(self.data.config.id)),
-            ("Center Number of Elements", stringify(self.data.config.num_elements)),
-            ("Center Functional Coefficients", stringify(self.data.coeffs_functional)),
-        ))
-
-        for e, (element_solver) in enumerate(self.element_solvers):
-            element_objective, dict_solved = element_solver.solve()
-
-            if element_objective == float("inf"):
-                print(f"\nNo optimal solution found for element {self.data.config.id}, "
-                      f"thus no solution found for center.")
-                return
-
-            element_solver.print_results()
-
-            center_functional += element_objective
-
-        print(f"\nCenter {stringify(self.data.config.id)} quality functional: {stringify(center_functional)}")
-
-    def validate_input(self) -> None:
-        """Validate the input data of the optimization for the center problem."""
-
-        assert_valid_dimensions(
-            [self.data.coeffs_functional,
-             self.data.elements, ],
-            [(self.data.config.num_elements,),
-             (self.data.config.num_elements,), ],
-            ["coeffs_functional",
-             "elements", ]
-        )
-
-        assert_positive(
-            self.data.config.num_elements,
-            "data.config.num_elements"
-        )
-        assert_non_negative(
-            self.data.config.id,
-            "data.config.id"
-        )
+        self.f_c_opt = [solver.setup() or solver.solve()[0] for solver in self.center_element_solvers]
--- comp/solvers/element/linear/second.py
+++ comp/solvers/element/linear/second.py
@@ -1,8 +1,8 @@
-from typing import List, Any, Dict
+from typing import Any, Dict, List
 
 from comp.models import ElementData
-from comp.solvers import ElementSolver
-from comp.utils import assert_valid_dimensions, assert_non_negative, assert_positive, tab_out, stringify
+from comp.solvers.core import ElementSolver
+from comp.utils import stringify, tab_out
 
 
 class ElementLinearSecond(ElementSolver):
@@ -11,16 +11,12 @@ class ElementLinearSecond(ElementSolver):
     def __init__(self, data: ElementData):
         super().__init__(data)
 
-        self.y_e: List[Any] = list()
         self.y_star_e: List[Any] = list()
 
     def setup_variables(self) -> None:
         """Set up optimization variables for the element problem."""
 
-        self.y_e = [
-            self.solver.NumVar(0, self.solver.infinity(), f"y_{self.data.config.id}_{i}")
-            for i in range(self.data.config.num_decision_variables)
-        ]
+        super().setup_variables()
 
         self.y_star_e = [
             self.solver.NumVar(0, self.solver.infinity(), f"y_star_{self.data.config.id}_{i}")
@@ -54,7 +50,7 @@ class ElementLinearSecond(ElementSolver):
         """
         Set up the objective function for the element problem.
 
-        max (c_e^T * y_star_e)
+        Max (c_e^T * y_star_e)
         """
 
         objective = self.solver.Objective()
@@ -75,7 +71,7 @@ class ElementLinearSecond(ElementSolver):
             "y_star_e": [v.solution_value() for v in self.y_star_e],
         }
 
-    def get_plan(self, pos: int) -> Any:
+    def get_plan_component(self, pos: int) -> Any:
         """Get the partial functional value for the element problem: c_e[pos]^T * y_star_e[pos]."""
 
         return self.y_star_e[pos]
@@ -83,81 +79,9 @@ class ElementLinearSecond(ElementSolver):
     def print_results(self) -> None:
         """Print the results of the optimization for the element problem."""
 
-        element_objective, dict_solved = self.solve()
+        super().print_results()
 
-        if element_objective == float("inf"):
-            print("\nNo optimal solution found.")
-            return
-
-        tab_out(f"\nInput data for element {stringify(self.data.config.id)}", (
-            ("Element Type", stringify(self.data.config.type)),
-            ("Element ID", stringify(self.data.config.id)),
-            ("Element Number of Decision Variables", stringify(self.data.config.num_decision_variables)),
-            ("Element Number of Constraints", stringify(self.data.config.num_constraints)),
-            ("Element Number of Schedules", stringify(self.data.config.num_schedules)),
-            ("Element Functional Coefficients", stringify(self.data.coeffs_functional)),
-            ("Element Resource Constraints", stringify(self.data.resource_constraints)),
-            ("Element Aggregated Plan Costs", stringify(self.data.aggregated_plan_costs)),
-            ("Element Delta", stringify(self.data.delta)),
-            ("Element W", stringify(self.data.w)),
-            ("Element Schedules", stringify(self.data.schedules)),
-            ("Element Interest", stringify(self.data.interest)),
-            ("Element Weight Coefficients", stringify(self.data.weight_coefficients)),
-        ))
-
-        tab_out(f"\nOptimization results for element {stringify(self.data.config.id)}", (
-            ("Decision Variables", stringify(dict_solved["y_e"])),
+        tab_out(f"Optimization results for element {stringify(self.data.config.id)}", (
+            ("Decision Variables", stringify((dict_solved := self.solve()[1])["y_e"])),
             ("Private Decision Variables", stringify(dict_solved["y_star_e"])),
         ))
-
-        print(f"\nElement {stringify(self.data.config.id)} quality functional: {stringify(element_objective)}")
-
-    def validate_input(self) -> None:
-        """Validate the input data of the optimization for the element problem."""
-
-        assert_valid_dimensions(
-            [self.data.coeffs_functional,
-             self.data.resource_constraints[0],
-             self.data.resource_constraints[1],
-             self.data.resource_constraints[2],
-             self.data.aggregated_plan_costs,
-             self.data.schedules,
-             self.data.interest,
-             self.data.weight_coefficients, ],
-            [(self.data.config.num_decision_variables,),
-             (self.data.config.num_constraints,),
-             (self.data.config.num_decision_variables,),
-             (self.data.config.num_decision_variables,),
-             (self.data.config.num_constraints, self.data.config.num_decision_variables),
-             (self.data.config.num_schedules,),
-             (self.data.config.num_constraints, self.data.config.num_schedules),
-             (self.data.config.num_constraints, self.data.config.num_schedules), ],
-            ["coeffs_functional",
-             "resource_constraints[0]",
-             "resource_constraints[1]",
-             "resource_constraints[2]",
-             "aggregated_plan_costs",
-             "schedules",
-             "interest",
-             "weight_coefficients", ]
-        )
-        assert_non_negative(
-            self.data.delta,
-            "data.delta"
-        )
-        assert_non_negative(
-            self.data.config.id,
-            "data.config.id"
-        )
-        assert_positive(
-            self.data.config.num_decision_variables,
-            "data.config.num_decision_variables"
-        )
-        assert_positive(
-            self.data.config.num_constraints,
-            "data.config.num_constraints"
-        )
-        assert_positive(
-            self.data.config.num_schedules,
-            "data.config.num_schedules"
-        )
