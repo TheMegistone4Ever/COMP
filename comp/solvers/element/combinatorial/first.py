8fafdbfa8623420b3464a8257743d9ab0716270b
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2025-05-05 21:55:32 +0300

    Did refactor.

--- comp/solvers/element/combinatorial/first.py
+++ comp/solvers/element/combinatorial/first.py
@@ -1,4 +1,4 @@
-from typing import List, Any, Dict
+from typing import Any, Dict
 
 from comp.models import ElementData
 from comp.solvers import BaseSolver
@@ -9,57 +9,7 @@ class ElementCombinatorialFirst(BaseSolver):
     """Solver for element-level optimization problems. 1'st combinatorial model."""
 
     def __init__(self, data: ElementData):
-        super().__init__()
-        # Validate input dimensions
-        assert_valid_dimensions(
-            [data.coeffs_functional,
-             data.resource_constraints[0],
-             data.resource_constraints[1],
-             data.resource_constraints[2],
-             data.aggregated_plan_costs,
-             data.schedules,
-             data.interest,
-             data.weight_coefficients, ],
-            [(data.config.num_decision_variables,),
-             (data.config.num_constraints,),
-             (data.config.num_decision_variables,),
-             (data.config.num_decision_variables,),
-             (data.config.num_constraints, data.config.num_decision_variables),
-             (data.config.num_schedules,),
-             (data.config.num_constraints, data.config.num_schedules),
-             (data.config.num_constraints, data.config.num_schedules), ],
-            ["coeffs_functional",
-             "resource_constraints[0]",
-             "resource_constraints[1]",
-             "resource_constraints[2]",
-             "aggregated_plan_costs",
-             "schedules",
-             "interest",
-             "weight_coefficients", ]
-        )
-        assert_non_negative(
-            data.delta,
-            "data.delta"
-        )
-        assert_non_negative(
-            data.config.id,
-            "data.config.id"
-        )
-        assert_positive(
-            data.config.num_decision_variables,
-            "data.config.num_decision_variables"
-        )
-        assert_positive(
-            data.config.num_constraints,
-            "data.config.num_constraints"
-        )
-        assert_positive(
-            data.config.num_schedules,
-            "data.config.num_schedules"
-        )
-
-        self.data = data
-        self.y_e: List[Any] = list()
+        super().__init__(data)
 
     def setup_variables(self) -> None:
         """Set up optimization variables for the element problem."""
@@ -89,3 +39,53 @@ class ElementCombinatorialFirst(BaseSolver):
         """Print the results of the optimization for the element problem."""
 
         raise NotImplementedError("print_results method not implemented")
+
+    def validate_input(self) -> None:
+        """Validate the input data of the optimization for the center problem."""
+
+        assert_valid_dimensions(
+            [self.data.coeffs_functional,
+             self.data.resource_constraints[0],
+             self.data.resource_constraints[1],
+             self.data.resource_constraints[2],
+             self.data.aggregated_plan_costs,
+             self.data.schedules,
+             self.data.interest,
+             self.data.weight_coefficients, ],
+            [(self.data.config.num_decision_variables,),
+             (self.data.config.num_constraints,),
+             (self.data.config.num_decision_variables,),
+             (self.data.config.num_decision_variables,),
+             (self.data.config.num_constraints, self.data.config.num_decision_variables),
+             (self.data.config.num_schedules,),
+             (self.data.config.num_constraints, self.data.config.num_schedules),
+             (self.data.config.num_constraints, self.data.config.num_schedules), ],
+            ["coeffs_functional",
+             "resource_constraints[0]",
+             "resource_constraints[1]",
+             "resource_constraints[2]",
+             "aggregated_plan_costs",
+             "schedules",
+             "interest",
+             "weight_coefficients", ]
+        )
+        assert_non_negative(
+            self.data.delta,
+            "data.delta"
+        )
+        assert_non_negative(
+            self.data.config.id,
+            "data.config.id"
+        )
+        assert_positive(
+            self.data.config.num_decision_variables,
+            "data.config.num_decision_variables"
+        )
+        assert_positive(
+            self.data.config.num_constraints,
+            "data.config.num_constraints"
+        )
+        assert_positive(
+            self.data.config.num_schedules,
+            "data.config.num_schedules"
+        )
