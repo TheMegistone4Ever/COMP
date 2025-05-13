604ed51acee186aa8ba9a46f9f1fcfeb060fc017
Кисельов Микита Євгенович <zeusmobilenick@gmail.com>
2025-05-13 15:35:17 +0300

    Added Unit-Tests.

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+from .test_core import (TestAssertions, TestHelpers, TestJsonSerializer, TestModels, TestGenerator, TestParallelization,
+                        TestSolversFactories, TestSolvers)
+
+__all__ = [
+    "TestAssertions",
+    "TestHelpers",
+    "TestJsonSerializer",
+    "TestModels",
+    "TestGenerator",
+    "TestParallelization",
+    "TestSolversFactories",
+    "TestSolvers",
+]
